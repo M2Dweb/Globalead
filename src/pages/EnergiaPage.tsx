12b83505d65efc3fed29626030f52db3748f4a7f
M2Dweb <geral.m2d.sites@gmail.com>
2025-07-15 23:58:52 +0100

    Updated EnergiaPage.tsx

--- src/pages/EnergiaPage.tsx
+++ src/pages/EnergiaPage.tsx
@@ -81,9 +81,8 @@ const EnergiaPage: React.FC = () => {
             </p>
           </div>
         </div>
-        </div>
       </section>
-
+    </div>
       {/* Simulation Form */}
       <section className="py-20 bg-gray-50">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
