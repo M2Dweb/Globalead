de318e25b4a13b45ef06e4c5a7de948c451cc4fe
M2Dweb <geral.m2d.sites@gmail.com>
2025-07-21 13:44:18 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
       case 'home':
         return <HomePage onNavigate={setCurrentPage} />;
       case 'sobre':
-        return <SobrePage />;
+        return <HomePage />;
       case 'imoveis':
         return <ImoveisPage />;
       case 'tv-net-voz':
