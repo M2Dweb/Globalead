fa24bb3ded45d4a466ed245002ffa3a4d7dd2eee
Vinhas <vinhasuvas59@gmail.com>
2025-08-30 16:05:34 +0100

    "Footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,17 +22,18 @@ const Footer: React.FC = () => {
 
           {/* Facebook Page Plugin */}
           <div>
-            <div className="bg-gray-800 p-4 rounded-lg mb-6">
+            <div className="bg-gray-800 rounded-lg overflow-hidden" style={{ height: '130px' }}>
               <iframe
+                className="focus:outline-none"
                 title="Globalead Facebook Page"
-                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fglobalead.pt&tabs=timeline&width=340&height=150&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fglobalead.pt&tabs=timeline&width=340&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
                 width="100%"
-                height="150"
+                height="130"
                 style={{ border: 'none', overflow: 'hidden' }}
                 scrolling="no"
                 frameBorder="0"
-                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-              ></iframe>
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+                </iframe>
             </div>
 
             {/* Redes Sociais com React Icons */}
