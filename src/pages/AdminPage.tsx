f7e97004d664ae89c95fb56110e8a891308a7eb2
Vinhas19 <vinhasuvas59@gmail.com>
2025-09-07 21:07:52 +0000

    "commit"

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -557,8 +557,8 @@ Conteúdo: ${post.content}
 
   return (
     <div className="min-h-screen bg-gray-100">
-      {/* Header */}
-      <div className="bg-white shadow-sm">
+      {/* Header do Admin */}
+      <div className="bg-white shadow-sm mt-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="py-6 flex justify-between items-center">
             <div>
@@ -574,6 +574,7 @@ Conteúdo: ${post.content}
           </div>
         </div>
       </div>
+
 
       {/* Tabs */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
